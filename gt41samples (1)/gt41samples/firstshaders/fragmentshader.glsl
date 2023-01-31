2f764baf26ab4dfce490929dbb96a0d4634be988
MehadND <34424878+MehadND@users.noreply.github.com>
2023-01-31 10:48:26 +0000

    added firstshader project

---
+++ gt41samples (1)/gt41samples/firstshaders/fragmentshader.glsl
@@ -0,0 +1,8 @@
+#version 330
+
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(0.380, 0.0950, 0.0950, 1.0);
+}
---
+++ gt41samples (1)/gt41samples/firstshaders/myShader.h
@@ -0,0 +1,13 @@
+// do same as primitive topologies tasks
+// read slides
+// step through code
+
+//learnopengl.com
+//ogldev.org
+
+
+#pragma once
+class myShader
+{
+};
+
